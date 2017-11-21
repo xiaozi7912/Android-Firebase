4218902d0a80cac7379546b79e886b3ec223cf2e
Larry <xiaozi7912@gmail.com>
2017-11-21 11:34:40 +0800

    [Title] Add Firebase Dynamic Link [Link] [Symptom] NA [Root Cause] NA [Solution] NA

--- app/src/main/java/com/xiaozi/android/firebase/MainActivity.java
+++ app/src/main/java/com/xiaozi/android/firebase/MainActivity.java
@@ -1,11 +1,21 @@
 package com.xiaozi.android.firebase;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 import com.xiaozi.framework.libs.BaseActivity;
 import com.xiaozi.framework.libs.utils.Logger;
 
 public class MainActivity extends BaseActivity {
+    private TextView mClinicIdTextView = null;
+
+    private String mClinicId = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +24,27 @@ public class MainActivity extends BaseActivity {
 
         initialize();
         initView();
+
+        FirebaseDynamicLinks.getInstance().getDynamicLink(getIntent())
+                .addOnSuccessListener(new OnSuccessListener<PendingDynamicLinkData>() {
+                    @Override
+                    public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
+                        Logger.i(LOG_TAG, "getDynamicLink onSuccess");
+                        Uri deepLink = null;
+                        if (pendingDynamicLinkData != null) {
+                            deepLink = pendingDynamicLinkData.getLink();
+                            mClinicId = deepLink.getQueryParameter("clinic_id");
+                            mClinicIdTextView.setText(mClinicId);
+                        }
+                        Logger.d(LOG_TAG, "getDynamicLink onSuccess deepLink : " + deepLink);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Logger.i(LOG_TAG, "getDynamicLink onFailure");
+                    }
+                });
     }
 
     @Override
@@ -25,5 +56,9 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void initView() {
         super.initView();
+        Logger.i(LOG_TAG, "initView");
+        mClinicIdTextView = findViewById(R.id.main_clinic_id_text);
+
+        mClinicIdTextView.setText("");
     }
 }
